dc1308e252f1f65f82ab82236a1636a306642be0
cjogles <jackson.ogles28@gmail.com>
2019-12-10 12:16:23 -0700

    added database file to data folder

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data.car-dealer.db3'
+      filename: './data/car-dealer.db3'
     },
     useNullAsDefault: true, // only needed when using sqlite3
     migrations: {
