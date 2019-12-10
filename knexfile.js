a4f0c3b218ae1243cd6be6a106b6863d5484e475
cjogles <jackson.ogles28@gmail.com>
2019-12-10 10:41:49 -0700

    added migrations and seeds to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,15 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.car-dealer.db3'
+      filename: './data.car-dealer.db3'
     },
-    useNullAsDefault: true // only needed when using sqlite3
+    useNullAsDefault: true, // only needed when using sqlite3
+    migrations: {
+      directory: './data/migrations' // will be created automatically
+    },
+    seeds: {
+      directory: './data/seeds' // will be created automatically
+    }
   }
 
 };
